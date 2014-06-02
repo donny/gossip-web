7a7118e91fa971985194dccb14dd059d2c31672d
Donny Kurniawan <donny.kurniawan@gmail.com>
2014-06-02 20:32:14 +1000

    Encode/decode JSON objects.

--- app/scripts/controllers/talk.js
+++ app/scripts/controllers/talk.js
@@ -9,10 +9,10 @@ angular.module('gossipWebApp')
   	$scope.userName = $cookies.userName;
     $scope.userColor = $cookies.userColor;
 
-  	$scope.appendText = function(text) {
-  		var texts = text.split(":::");
-      $scope.data.lines.push({title: texts[0], style: {color: texts[1]}, content: texts[2]});
-  		$scope.$digest();
+  	$scope.appendTextData = function(textData) {
+      var data = JSON.parse(textData);
+      $scope.data.lines.push({title: data.Name, style: {color: data.Color}, content: data.Text});
+  		$scope.$digest(); // Because we are called from outside Angular (WebSocket.onmessage)
   	};
 
   	$scope.appendInfo = function(text) {
@@ -23,8 +23,11 @@ angular.module('gossipWebApp')
     	if ($scope.socketConnection === null) {
     		$scope.appendInfo("Your browser does not support WebSockets.");
     	} else {
-        var text = $scope.userName + ':::' + $scope.userColor + ':::' + $scope.data.text;
-	    	$scope.socketConnection.send(text);
+        var data = {};
+        data.Name = $scope.userName;
+        data.Color = $scope.userColor;
+        data.Text = $scope.data.text;
+	    	$scope.socketConnection.send(JSON.stringify(data));
     	}
 
     	$scope.data.text = "";
@@ -37,7 +40,7 @@ angular.module('gossipWebApp')
 	            $scope.appendInfo("Connection closed.");
 	        };
 	        $scope.socketConnection.onmessage = function(evt) {
-	            $scope.appendText(evt.data);
+	            $scope.appendTextData(evt.data);
 	        };
 	    } else {
 	        $scope.appendInfo("Your browser does not support WebSockets.");
